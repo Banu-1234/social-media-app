e7d91a637c829ec980f23da0381cb65747e153d1
Banu-1234 <banupalla1@gmail.com>
2025-09-09 14:34:26 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true }, // Ensure unique usernames
   bio: String
-})
+}, { timestamps: true }) // Adds createdAt and updatedAt timestamps
 
 module.exports = mongoose.model("User", userSchema)
